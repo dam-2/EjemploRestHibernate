18ee78c1678406d159dc3b9edd9b4d9971a10582
Sergio Martínez <smlara@gmail.com>
2019-01-31 22:55:17 +0100

    Ejemplo Completado. Lista Empleados, consulta un Empleado y Actulizar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EjemploRestHibernate
+
+Este es un ejemplo completo del uso del protocolo Rest para actualizar una base de datos usando Hibernate como motor de persistencia
+
--- src/main/java/ejemplos2DAM/EjemploRestHibernate/EmpleadosRest.java
+++ src/main/java/ejemplos2DAM/EjemploRestHibernate/EmpleadosRest.java
@@ -2,11 +2,19 @@ package ejemplos2DAM.EjemploRestHibernate;
 
 import java.util.List;
 
+import javax.persistence.PersistenceException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.hibernate.*;
 
@@ -20,6 +28,12 @@ import ejemplos2DAM.Entidades.*;
 public class EmpleadosRest {
 	private static SessionFactory sesFactory = HibernateUtil.getSessionFactory();
 	
+    @Context
+    private UriInfo uriInfo;
+    @Context
+    private Request request;
+
+	
     /**
      * Método para devolver los empleados en formato XML
      * @return String that will be returned as a text/plain response.
@@ -29,11 +43,14 @@ public class EmpleadosRest {
     public static SessionFactory getSesFactory() {
 		return sesFactory;
 	}	
-	
-	
+		
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_XML)
+    /**
+     * Devuelve una lista de empleados
+     * @return
+     */
     public List<Empleados> getEmpleados()
     {
     	Session session = sesFactory.openSession();
@@ -55,6 +72,11 @@ public class EmpleadosRest {
 	@GET
     @Path("/{empleadoId}")
     @Produces(MediaType.APPLICATION_XML)
+	/**
+	 * Devuelve un solo empleado especificado en la url
+	 * @param empleadoId
+	 * @return
+	 */
     public Empleados getEmpleado(@PathParam("empleadoId") int empleadoId)
     {
     	Session session = sesFactory.openSession();
@@ -72,4 +94,32 @@ public class EmpleadosRest {
 		return empleado;
     	
     }
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	/**
+	 * Actualiza la BBDD con el contenido XML 
+	 * 
+	 * @param departamento
+	 * @return
+	 */
+	public Response postEmpleado(Empleados empleado) {
+		Session session = sesFactory.openSession();
+		Response res;
+
+	//	System.out.println(departamento.toString());
+		try {
+			Transaction tx = session.beginTransaction();
+			session.saveOrUpdate(empleado); // Crear un nuevo registro o actulizar el existente
+			tx.commit();
+            res = Response.created(uriInfo.getAbsolutePath()).build();
+		} catch (PersistenceException e) {
+				e.printStackTrace();
+			 res = Response.noContent().build();
+		} finally {
+			session.close();
+		}		
+
+		return res;
+	}
 }
